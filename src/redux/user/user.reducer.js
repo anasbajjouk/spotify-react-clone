5a33ef6139e9cc747c82e98fa921e69eb7fb52dd
anasbajjouk <a.bajjouk@cleo.one>
2020-11-06 19:39:27 +0100

    minor fix

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,8 +2,7 @@ import { UserActionTypes } from './user.types'
 
 const INITIAL_STATE = {
   currentUser: null,
-  token:
-    'BQDmPD88HM79igxc_1D2mp73vXSqpk-EnFUCgbfDVdnEC2mPN6fetP4Re20yhjhnSa5nmqkOldwbNLNC01EhHLmV4uuLVtknO52b7VImAxfgHV40A8zdNX1LLGGIO-I9wqyh6X6qUhI182n3fwpeYEqiAiujwKAP8HLiWe0HmKxXx_F6r7t4jcZzSDntLF2ObiQPAS3oyKXEIimnA29En0kxhLZ1xwrnZXmqo--QQy8p6Uwyo7ue9Qwr3kCgJLgMYU-o4fwPh7M8e_8JPXg0mzu-VS2JWZdz',
+  token: ''
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {

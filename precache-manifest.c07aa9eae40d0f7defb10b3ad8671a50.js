5be35ebe499c01147d38a5c11eb904dbf527984c
anasbajjouk <a.bajjouk@cleo.one>
2020-11-06 19:56:01 +0100

    Updates

---
+++ precache-manifest.c07aa9eae40d0f7defb10b3ad8671a50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba1468eca116a30693bb4a1a2492d1e",
+    "url": "/anasbajjouk/spotify-react-clone/index.html"
+  },
+  {
+    "revision": "ff001b5766f7d4ad6416",
+    "url": "/anasbajjouk/spotify-react-clone/static/css/main.b6d1f9d8.chunk.css"
+  },
+  {
+    "revision": "86cb59aa779c03492f66",
+    "url": "/anasbajjouk/spotify-react-clone/static/js/2.427e6333.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/anasbajjouk/spotify-react-clone/static/js/2.427e6333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff001b5766f7d4ad6416",
+    "url": "/anasbajjouk/spotify-react-clone/static/js/main.eb6f2c56.chunk.js"
+  },
+  {
+    "revision": "7bf557e944a2803fee9e",
+    "url": "/anasbajjouk/spotify-react-clone/static/js/runtime-main.355faf1c.js"
+  },
+  {
+    "revision": "7db352f35b623f1b3dd850ca105013cb",
+    "url": "/anasbajjouk/spotify-react-clone/static/media/Spotify_Logo_RGB_Green.7db352f3.svg"
+  },
+  {
+    "revision": "22bed0336e8f552e79cab88d0c2f0947",
+    "url": "/anasbajjouk/spotify-react-clone/static/media/Spotify_Logo_RGB_White.22bed033.svg"
+  }
+]);
